8b69fa799345f8cc4b6cebb80a4933c411c84232
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2021-12-27 11:50:30 -0700

    Create useful_r_functions.R

---
+++ useful_r_functions.R
@@ -0,0 +1,9 @@
+# Quick proportion for binary variables
+bin_prop <- function(x, na_opt = FALSE){
+  if (na_opt == FALSE) {
+    table(x)[2] / sum(table(x))
+  }
+  else {
+    table(x)[2] / sum(table(x, useNA = 'ifany'))
+  }
+}
