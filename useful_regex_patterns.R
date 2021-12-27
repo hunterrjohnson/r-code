596a27b5aaca336cdb53dd65a08a6eecd2395ae0
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2021-12-27 16:24:05 -0700

    Update useful_regex_patterns.R

--- useful_regex_patterns.R
+++ useful_regex_patterns.R
@@ -1,2 +1,5 @@
 # Replace excessive whitespace with single space including in between strings
 gsub("\\s+", " ", str_trim(df$col_1))
+
+# Extract everything before first underscore not including underscore
+as.character(stringr::str_extract_all(df$col_1, "^[^_]+(?=_)"))
