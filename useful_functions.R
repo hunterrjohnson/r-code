e4ad96dc67cad4a06c07a0bd2229d6d76f719b40
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2022-01-13 10:37:53 -0700

    Update useful_functions.R

--- useful_functions.R
+++ useful_functions.R
@@ -1,3 +1,8 @@
+# Function for "not in"
+'%!in%' <- function(x,y){
+  !('%in%'(x,y))
+}
+
 # Quick proportion for binary variables
 bin_prop <- function(x, na_opt = FALSE){
   if (na_opt == FALSE) {
