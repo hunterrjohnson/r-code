4df0070c0fcf7917f1a1e2107148a9f074c731ad
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2022-02-01 15:01:10 -0700

    Update useful_regex_patterns.R

--- useful_regex_patterns.R
+++ useful_regex_patterns.R
@@ -3,3 +3,6 @@ gsub("\\s+", " ", str_trim(df$col_1))
 
 # Extract everything before first underscore not including underscore
 as.character(stringr::str_extract_all(df$col_1, "^[^_]+(?=_)"))
+
+# Create flag to remove any apartment units from addresses
+df$drop <- ifelse(grepl("(?i)(APT|UNIT|#|STE)\\s?[a-zA-Z0-9]+", df$address), 1, 0)
