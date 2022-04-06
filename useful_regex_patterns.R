96fa863325df46921fac9bf65dff07ed0412bba3
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2022-04-06 13:38:13 -0700

    Update useful_regex_patterns.R

--- useful_regex_patterns.R
+++ useful_regex_patterns.R
@@ -5,4 +5,12 @@ gsub("\\s+", " ", str_trim(df$col_1))
 as.character(stringr::str_extract_all(df$col_1, "^[^_]+(?=_)"))
 
 # Create flag to remove any apartment units from addresses
-df$drop <- ifelse(grepl("(?i)(APT|UNIT|#|STE)\\s?[a-zA-Z0-9]+", df$address), 1, 0)
+df$drop <- ifelse(grepl("(?i)(APT|UNIT|#|STE|SPC|BLDG)\\s?[a-zA-Z0-9]+", df$address), 1, 0)
+df$drop <- ifelse(grepl("(\\s)(?i)(APT|UNIT|STE|SUITE)(\\s)(.*)", df$Street), 1, 0) # need to be followed by a space
+df$drop <- ifelse(grepl("(\\s)(?i)(#|SPC|BLDG|BLDNG)(.*)", df$Street), 1, df$drop) # don't need to be followed by a space
+df$drop <- ifelse(grepl("(\\s)(?i)(LOWR|UPPR)(.*)", df$Street), 1, df$drop) # rare weird case in meter metadata
+df$drop <- ifelse(grepl("(\\s)(?i)(HSE)(.*)", df$Street), 1, df$drop) # rare weird case in meter metadata
+df$drop <- ifelse(grepl("(\\s)(/)(.*)", df$Street), 1, df$drop) # rare weird case in meter metadata
+df$drop <- ifelse(grepl("[ ]\\(.*", df$Street), 1, 0) # for checking parentheses
+df$drop <- ifelse(grepl("(\\-)(\\s?)[a-zA-Z]+(.*)", df$Street), 1, 0) # for checking hyphens
+df$drop <- ifelse(grepl("(\\-)$", df$Street), 1, df$drop)
